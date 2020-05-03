649e5c9a0bd7b846e74c19517a34fe0e934d9608
DominiqueYoumans <ddyoumans@icloud.com>
2020-05-03 13:19:25 -0700

    add form visible reducer to test folder.

--- src/_tests_/reducers/keg-list-reducer.test.js
+++
@@ -1,7 +0,0 @@
-import tapListReducer from '../../reducers/keg-list-reducer';
-
-describe('kegListReducer', () => {
-    test('Should return default state if there is no action type passed into the reducer', () => {
-        expect(kegListReducer({}, { type: null })).toEqual({});
-    });
-});
