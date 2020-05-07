859851afd70ae409b38e9d4b0053341f23a7a409
DominiqueYoumans <ddyoumans@icloud.com>
2020-05-06 20:58:20 -0700

    add test description.

--- src/_tests_/reducers/index-reducer.test.js
+++ src/_tests_/reducers/index-reducer.test.js
@@ -7,5 +7,9 @@ import * as c from './../../actions/ActionTypes';
 let store = createStore(rootReducer);
 
 describe("rootReducer", () => {
-    
+
+    test('Should return default state if no action type is recognized', () => {
+        
+    })
+
 })
