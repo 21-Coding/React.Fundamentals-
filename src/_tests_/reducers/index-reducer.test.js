955129def21107136db027fec563548817056555
DominiqueYoumans <ddyoumans@icloud.com>
2020-05-06 20:59:07 -0700

    complete ending brackets.

--- src/_tests_/reducers/index-reducer.test.js
+++ src/_tests_/reducers/index-reducer.test.js
@@ -9,7 +9,7 @@ let store = createStore(rootReducer);
 describe("rootReducer", () => {
 
     test('Should return default state if no action type is recognized', () => {
-        
-    })
 
-})
+    });
+
+});
