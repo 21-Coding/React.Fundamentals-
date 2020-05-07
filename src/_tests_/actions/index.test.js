4ee2fecb261410edfe40bd8aba55fb75f85982ff
DominiqueYoumans <ddyoumans@icloud.com>
2020-05-06 21:10:41 -0700

    add new actions directory with describe and it statements for test.

---
+++ src/_tests_/actions/index.test.js
@@ -0,0 +1,8 @@
+import * as actions from './../../actions';
+import * as c from './../../actions/ActionTypes';
+
+describe('keg tracker actions', () => {
+    it('addKeg should ADD_KEG action', () => {
+
+    });
+});
